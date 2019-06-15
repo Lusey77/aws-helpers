ec23b3bd479fa271bac26e9a688d93669e22def7
Lusey77 <Lusey77@users.noreply.github.com>
2019-06-15 10:05:52 -0500

    etc: Testing S3 versioning etc: Ran npm install feat: Added types to event and context in lambda handler

--- lambda-deploy/index.ts
+++ lambda-deploy/index.ts
@@ -1,5 +1,7 @@
-export const handler = async (event: any, context: any): Promise<any> => {
-    console.log(`Attempting to deploy app`);
+import { Context, S3EventRecord } from 'aws-lambda';
+
+export const handler = async (event: S3EventRecord, context: Context): Promise<string> => {
+    console.log(`Attempting to deploy app-v2`);
     console.log(`Event: ${JSON.stringify(event)}`);
     console.log(`Context: ${JSON.stringify(context)}`);
 
