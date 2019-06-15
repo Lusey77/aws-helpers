c071b801f89b2aa2c711897755ce07671f00e24a
Lusey77 <Lusey77@users.noreply.github.com>
2019-06-15 10:15:56 -0500

    etc: Testing lambda versioning

--- lambda-deploy/index.ts
+++ lambda-deploy/index.ts
@@ -1,7 +1,7 @@
 import { Context, S3EventRecord } from 'aws-lambda';
 
 export const handler = async (event: S3EventRecord, context: Context): Promise<string> => {
-    console.log(`Attempting to deploy app-v2`);
+    console.log(`Attempting to deploy app-v3`);
     console.log(`Event: ${JSON.stringify(event)}`);
     console.log(`Context: ${JSON.stringify(context)}`);
 
